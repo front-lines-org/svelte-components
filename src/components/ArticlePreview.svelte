cf8913379ab8ab5b21ac4b1170e85d2d9a9c2894
Ruben Soerdien <ruben.soerdien@hotmail.com>
2022-10-29 17:19:21 +0200

    initial commit

---
+++ src/components/ArticlePreview.svelte
@@ -0,0 +1,45 @@
+<script>
+    export let title;
+    export let description;
+    export let author;
+    export let url;
+    export let img;
+    export let publishDate;
+    export let showExcerpt;
+
+    export let size = 'md';
+</script>
+
+<a href={url} class="hover:underline">
+    <div class="flex flex-wrap justify-evenly">
+        {#if size !== 'xs'}
+            <div class={`image-${size}`}>
+                <img src={img && img.startsWith('http') ? img : `/images/${img}`} alt="" />
+            </div>
+        {/if}
+        <div class="item w-32 h-44 flex-grow p-4">
+            {#if size !== 'xs'}
+                <p style="color: #f9a1bc;" class="font-black">Ukraine</p>
+            {/if}
+            <h1 class="text-gray-900 font-bold py-2 text-lg ">{title}</h1>
+            {#if description && showExcerpt && size != 'xs'}
+                <p class="font-normal text-gray-400">{description}</p>
+            {/if}
+            {#if publishDate}
+                <p class="font-normal text-gray-400">{new Date(publishDate).toLocaleString()}</p>
+            {/if}
+            {#if author && size != 'xs'}
+                <p class="font-bold py-1 text-gray-600 text-sm">By {author}</p>
+            {/if}
+        </div>
+    </div>
+</a>
+
+<style>
+    .image-sm {
+        @apply w-44 my-auto;
+    }
+    .image-md {
+        @apply w-full my-auto;
+    }
+</style>
---
+++ src/stories/ArticlePreview.stories.svelte
@@ -0,0 +1,73 @@
+<script>
+    import {Meta, Template, Story} from '@storybook/addon-svelte-csf';
+    import ArticlePreview from '../components/ArticlePreview.svelte';
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<!--     
+    export let title: " a",
+    export let description: " a",
+    export let author: " a",
+    export let url: " a",
+    export let img: " a",
+    export let showExcerpt: " a",
+ -->
+<Meta
+    title="Example/ArticlePreview"
+    component={ArticlePreview}
+    argTypes={{
+        title: {control: 'text'},
+        description: {control: 'text'},
+        author: {control: 'text'},
+        url: {control: 'text'},
+        img: {control: 'text'},
+        showExcerpt: {control: 'boolean'},
+        publishDate: {control: 'date'},
+        size: {
+            control: {type: 'select'},
+            options: ['xs', 'sm', 'md', 'lg', 'xl']
+        }
+    }}
+/>
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+    <ArticlePreview {...args} on:click={args.onClick} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story
+    name="Primary"
+    args={{
+        title: 'Battle of Antonov Airport',
+        description:
+            'The Battle of Antonov Airport, also known as the Battle of Hostomel Airport, was a military engagement which occurred at the Antonov Airport in Hostomel, Kyiv Oblast, during the Kyiv offensive of the 2022 Russian invasion of Ukraine.',
+        author: 'Wikipedia',
+        // url: '',
+        img: 'https://test.front-lines.org/images/ukraine(63)-ejs-1280.webp',
+        showExcerpt: true
+    }}
+/>
+
+<Story
+    name="Secondary"
+    args={{
+        label: 'Button'
+    }}
+/>
+<Story
+    name="Large"
+    args={{
+        size: 'large',
+        label: 'Button'
+    }}
+/>
+
+<Story
+    name="Small"
+    args={{
+        size: 'small',
+        label: 'Button'
+    }}
+/>
