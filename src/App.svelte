1b971784ca2a8423a115d10a31b2b35cf342c527
Ruben Soerdien <ruben.soerdien@hotmail.com>
2022-10-29 16:38:07 +0200

    init repo

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Nav from './components/nav.svelte';
+    import Button from './components/Button.svelte';
+    let menuItems = [
+        {link: '#', name: 'Docs'},
+        {link: '#', name: 'Home'},
+        {link: '#', name: 'Blog'}
+    ];
+</script>
+
+<Nav {menuItems} />
+<Button text="Something" />
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
